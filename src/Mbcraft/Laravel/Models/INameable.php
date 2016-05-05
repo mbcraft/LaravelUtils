231b4e9a3a13e831de72f4efe4dfd7090bf3f131
mbcraft <info@mbcraft.it>
2016-05-05 09:58:14 +0200

    More documentation.

--- src/Mbcraft/Laravel/Models/INameable.php
+++ src/Mbcraft/Laravel/Models/INameable.php
@@ -2,20 +2,24 @@
 
 namespace Mbcraft\Laravel\Models;
 
+/**
+ * This interface must be implemented by model classes to be used with the entity controllers.
+ */
 interface INameable {
     
     /**
-     * Returns the name of one entity used in controllers and views.
+     * Returns the name of one entity of this model (singular).
+     * Example : "invoice"
      * 
-     * @return string The name of one entity
+     * @return string The string name of one entity.
      */
     public static function one_entity();
     
     /**
-     * Ritorna il nome di molte entità.
+     * Returns the string used to identify many entities of this model (plural).
+     * Example : "invoices"
      * 
-     * @return string Ritorna il nome di molte entità come stringa, 
-     *      in questo caso "spots".
+     * @return string The string name of many entities.
      */
     public static function many_entities();
     
