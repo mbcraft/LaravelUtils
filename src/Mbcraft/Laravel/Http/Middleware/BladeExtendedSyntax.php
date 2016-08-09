fec0cb9cab6fe8b7c775f58a41da672c42b599be
mbcraft <info@mbcraft.it>
2016-08-09 18:29:36 +0200

    Fixed @if_has_elements to check if the variable is set

--- src/Mbcraft/Laravel/Http/Middleware/BladeExtendedSyntax.php
+++ src/Mbcraft/Laravel/Http/Middleware/BladeExtendedSyntax.php
@@ -107,9 +107,9 @@ class BladeExtendedSyntax extends __AbstractBladeSyntax
     private function setupIfHasElements() {
         Blade::directive('if_has_elements', function($expression) {
             return '<?php '."\n"
-            .'$my_var = '.$this->stringParamAsString($expression).';'."\n"
-            .'$__array_with_items = isset($my_var) && is_array($my_var) && count($my_var)>0;'."\n"
-            .'$__collection_with_items = isset($my_var) && $my_var instanceof Illuminate\Database\Eloquent\Collection && count($my_var->count())>0;'."\n"
+            .'$my_var = isset('.$this->stringParamAsString($expression).') ? '.$this->stringParamAsString($expression).' : null;'."\n"
+            .'$__array_with_items = $my_var!=null && isset($my_var) && is_array($my_var) && count($my_var)>0;'."\n"
+            .'$__collection_with_items = $my_var!=null && isset($my_var) && $my_var instanceof Illuminate\Database\Eloquent\Collection && count($my_var->count())>0;'."\n"
             .'if ($__array_with_items || $__collection_with_items): '."\n"
             .'  unset($__array_with_items);'."\n"
             .'  unset($__collection_with_items);'."\n"
